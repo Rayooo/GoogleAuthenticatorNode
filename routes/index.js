59dbdf39c565b5c6715e8ab5e8220a7d124a3e25
Ray <chenxiaolei.ray@gmail.com>
2016-11-18 20:40:30 +0800

    完成验证功能

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,23 @@ var speakeasy = require("speakeasy");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var secret = speakeasy.generateSecret();
-    var url = speakeasy.otpauthURL({ secret: secret.ascii, label: 'Ray', algorithm: 'sha512' });
+    console.log(secret.base32);
+    var url = speakeasy.otpauthURL({ secret: secret.base32, label: 'Ray', algorithm: 'sha1' });
     res.render('index', { title: secret,qrcode: "http://qr.liantu.com/api.php?&w=400&text=" + url });
-
 });
+
+
+router.post("/verify",function (req, res, next) {
+    var code = req.body.code;
+    //这个key就是上面的secret.base32
+    var secretKey = "O56TMLSJMRLWKV2LLIUE423IEU4TUPZVIVTXILSUOY7FQPRJKIXQ";
+
+    var tokenValidates = speakeasy.totp.verify({
+        secret: secretKey,
+        token: code
+    });
+    console.log(tokenValidates);
+
+})
 
 module.exports = router;
