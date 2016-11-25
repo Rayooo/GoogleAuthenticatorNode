e372b9dafdd064083a1551f9bd2272044f61c904
Ray <chenxiaolei.ray@gmail.com>
2016-11-25 19:10:16 +0800

    完成前端注册登陆界面

--- public/js/particlesSetting.js
+++ public/js/particlesSetting.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 1,
+        "speed": 2,
         "direction": "none",
         "random": true,
         "straight": false,
