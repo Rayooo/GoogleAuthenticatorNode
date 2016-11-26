1d6381c0e10b265443b053c579283aa39416f702
Ray <chenxiaolei.ray@gmail.com>
2016-11-26 21:41:00 +0800

    修改index为静态文件

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,6 @@ var router = express.Router();
 var speakeasy = require("speakeasy");
 var passwordHash = require("password-hash");
 
-router.get('/', function(req, res, next) {
-    var secret = speakeasy.generateSecret();
-    console.log(secret.base32);
-    var url = speakeasy.otpauthURL({ secret: secret.base32, label: 'Ray-TomKK-WYH', algorithm: 'sha1' });
-    res.render('index', { title: secret,qrcode: "http://qr.liantu.com/api.php?&w=400&text=" + url });
-});
-
-
 router.post("/verify",function (req, res, next) {
     var code = req.body.code;
     //这个key就是上面的secret.base32
